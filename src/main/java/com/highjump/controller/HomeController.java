3165d239caf8412b27706808fb2a2d45cf7a5364
highjump <lixiang901112@163.com>
2017-03-04 13:11:17 +0800

    Added about page

--- src/main/java/com/highjump/controller/HomeController.java
+++ src/main/java/com/highjump/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 /**
  * Created by Administrator on 3/3/17.
  */
@@ -11,7 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Map<String, Object> model) {
+        model.put("page", "index");
+
         return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(Map<String, Object> model) {
+        model.put("page", "about");
+
+        return "aboutus";
     }
 }
